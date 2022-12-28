cda1283e84991b1b474707debe9d5abab842d1bd
MaratAkhmetzanov <ahmetzanovmarat@gmail.com>
2022-12-28 16:27:48 +0300

    comments

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Получение данных с по url через fetch (стырено из дз по js1 и измененно малость)
+ * @param {string} url - адрес куда отправляется запрос
+ * @returns
+ */
 const getData = async (url) =>
     await fetch(url, {
         method: 'GET',
@@ -10,11 +15,20 @@ const getData = async (url) =>
         }
     });
 
+/**
+ * Поисковой запрос на сервер по введенному тексту. Берем текст и получаем данные через getData и оттуда забираем только массив
+ * @param {string} searchText - то что ввели в строку поиска
+ * @returns - массив возможных вариантов
+ */
 const loadSearch = async (searchText) => {
     const result = await getData(`https://api.jikan.moe/v4/anime?limit=10&letter=${searchText}`);
     return result.data;
 };
 
+/**
+ * Функция обновляющая историю результатов на странице. Берём список из localSrorage и обновляем блок с историей.
+ * Работает при загрузке страницы и при добавлении элемента в историю.
+ */
 const updateSearhHistory = () => {
     const historyList = document.querySelector('.history__list');
     let searchHistory = JSON.parse(window.localStorage.getItem('searchHistory'));
@@ -37,6 +51,12 @@ const updateSearhHistory = () => {
     }
 };
 
+/**
+ * Добавляет в предложенные варианты поиска ещё и варианты из истории, если есть что-то подходящее.
+ * Также на каждый элемент вешается слушатель события клика.
+ * @param {*} searchResultslist - блок куда добавляются варианты.
+ * @param {string} value - то что ввели строку поиска
+ */
 const addHistoryToSearchList = (searchResultslist, value) => {
     const searchHistory = JSON.parse(window.localStorage.getItem('searchHistory')) || [];
     const historyMatch = searchHistory.filter((item) => item.title.toLowerCase().startsWith(value.toLowerCase()));
@@ -59,6 +79,11 @@ const addHistoryToSearchList = (searchResultslist, value) => {
     }
 };
 
+/**
+ * Обработчик нажатия на элемент в предложенных вариантах поиска (которые с сервера прилетели).
+ * Тут обновляем переменную в localStorage, добавляем результат поиска и обновляем историю поиска.
+ * @param {clickEvent} event 
+ */
 const searchResultClickHandler = (event) => {
     const { mal_id } = event.target.dataset;
     const searchInpit = document.getElementById('search__input');
@@ -82,6 +107,10 @@ const searchResultClickHandler = (event) => {
     searchInpit.value = event.target.innerText;
 };
 
+/**
+ * Загружаем данные с сервера по id и отображаем на странице.
+ * @param {number} id 
+ */
 const setResultData = async (id) => {
     const result = await getData(`https://api.jikan.moe/v4/anime/${id}`);
     document.querySelector('.content').classList.add('content_visible');
@@ -97,6 +126,7 @@ const searchResultslist = document.querySelector('.search__results-list');
 const searchResultsLoader = document.querySelector('.search__results-loader');
 const searchNoResults = document.querySelector('.search__no-results');
 
+// Основной скрипт, который обрабатываем ввод в поле поиска и загружает историю при старте.
 (async () => {
     updateSearhHistory();
     searchInpit.addEventListener('input', (event) => {
